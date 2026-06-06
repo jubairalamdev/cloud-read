60cdf1e6e77ab79c5a6bdd43978c93cd5476170d
Jubair Alam <jubairalam.dev@gmail.com>
2026-06-06 21:44:08 +0600

    Ui Dependency added + Theme setted to light mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "./Components/Shared/Navbar";
+import Footer from "./Components/Shared/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,9 +22,14 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      data-theme="light"
+      className={`${geistSans.variable} light ${geistMono.variable}`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="bg-background text-foreground">
+        <Navbar />
+          {children}
+        <Footer />
+      </body>
     </html>
   );
 }
