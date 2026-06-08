88f201271efd0bada1c4daa9d6786125033afdd6
Jubair Alam <jubairalam.dev@gmail.com>
2026-06-08 16:34:50 +0600

    A Borrow button in client side added in the Book Details page.

---
+++ src/Components/BookDetailsPage/BorrowButton.jsx
@@ -0,0 +1,20 @@
+"use client"
+import { toast } from 'react-toastify';
+
+const BorrowButton = ({book}) => {
+     const notifyBorrow = () => toast.success("Successfully Borrowed Book!");
+    return (
+        <button
+            onClick={notifyBorrow}
+            disabled={book.available_quantity === 0}
+            className={`flex-1 py-4 rounded-xl font-bold text-lg shadow-xl transition-all transform hover:scale-[1.02] cursor-pointer ${book.available_quantity > 0
+                    ? "bg-[#ff6b6b] hover:bg-[#e05e5e] text-white shadow-[#ff6b6b]/30"
+                    : "bg-gray-300 text-gray-500 cursor-not-allowed"
+                }`}
+        >
+            {book.available_quantity > 0 ? "Borrow Now" : "Out of Stock"}
+        </button>
+    );
+};
+
+export default BorrowButton;
