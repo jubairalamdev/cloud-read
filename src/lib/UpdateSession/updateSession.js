a18d6907bd0a8ba1bbfd81e5579a818e459e7153
Jubair Alam <jubairalam.dev@gmail.com>
2026-06-09 00:32:03 +0600

    update session and user data

---
+++ src/lib/UpdateSession/updateSession.js
@@ -0,0 +1,19 @@
+import { authClient } from "@/lib/auth-client"
+import { toast } from "react-toastify";
+
+export const updateSession = async (formData) => {
+    const action = "Profile Update"
+    console.log(formData)
+
+    try {
+        const user = Object.fromEntries(formData.entries());
+        await authClient.updateUser({
+            name: user.name,
+            image: user.photoUrl,
+        });
+        toast.success(`${action} Successful`)
+    }
+    catch (error) {
+        toast.error("There was a Server Error")
+    }
+}
